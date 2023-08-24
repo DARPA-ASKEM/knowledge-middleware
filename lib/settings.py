f70dbef9f53e7deb6531a0ef2ce3603918291c43
Five Grant <5@fivegrant.com>
2023-08-23 22:05:40 -0500

    Switch internal settings

--- lib/settings.py
+++ lib/settings.py
@@ -4,10 +4,10 @@ class Settings(BaseSettings):
     LIVE: bool = False
     REDIS_HOST: str = "redis.ta1-service"
     REDIS_PORT: int = 6379
-    TA1_UNIFIED_URL: str = "mock://ta1:5"
-    SKEMA_RS_URL: str = "mock://skema-rs.staging.terarium.ai"
-    MIT_TR_URL: str = "mock://mit:10"
-    TDS_URL: str = "mock://tds:15"
+    TA1_UNIFIED_URL: str = "http://ta1:5"
+    SKEMA_RS_URL: str = "http://skema-rs.staging.terarium.ai"
+    MIT_TR_URL: str = "http://mit:10"
+    TDS_URL: str = "http://tds:15"
     OPENAI_API_KEY: str = "foo"
     LOG_LEVEL: str = "INFO"
 
